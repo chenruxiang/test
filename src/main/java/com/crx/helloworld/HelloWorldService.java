a044d76b45d9f8641f79b6bd46e0a28adb3c9646
jftcrx <604382226@qq.com>
2018-10-11 14:46:57 +0800

    gg

--- src/main/java/com/crx/helloworld/HelloWorldService.java
+++ src/main/java/com/crx/helloworld/HelloWorldService.java
@@ -1,16 +1,23 @@
 package com.crx.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Administrator on 2018/10/10 0010.
  */
+@Component
 public class HelloWorldService {
+
+    @Autowired
     private HelloWorld helloWorld;
 
     public HelloWorld getHelloWorld() {
         return helloWorld;
     }
 
-    public void setHelloWorld(HelloWorld helloWorld) {
+    /*public void setHelloWorld(HelloWorld helloWorld) {
         this.helloWorld = helloWorld;
-    }
+    }*/
 }
--- src/main/java/com/crx/helloworldimpl/SpringHelloWorld.java
+++ src/main/java/com/crx/helloworldimpl/SpringHelloWorld.java
@@ -1,13 +1,15 @@
 package com.crx.helloworldimpl;
 
 import com.crx.helloworld.HelloWorld;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Administrator on 2018/10/10 0010.
  */
+@Component
 public class SpringHelloWorld implements HelloWorld {
     @Override
     public void sayHello() {
-        System.out.println("Spring Hello World!");
+        System.out.println("Spring Hello World!！");
     }
 }
